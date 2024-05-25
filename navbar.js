f5ed7492ad6fe9fbe25857a5864afb757366cd8e
KraftaN <63770425+KrftaN@users.noreply.github.com>
2024-05-25 20:55:58 +0200

    final commit

--- navbar.js
+++ navbar.js
@@ -1,15 +1,17 @@
-function init (){
-	const body = document.querySelector("body");
-	const menu = document.querySelector(".menu-icon");
+function init (){ /* denna funktion innehåller all JS kod */
+	const body = document.querySelector("body"); /* Detta kallar body elementet */
+	const menu = document.querySelector(".menu-icon"); /* Detta kallar meny ikonen */
 
-	menu.addEventListener("click", () => {
-		toggleNav();
+	menu.addEventListener("click", () => { /* Detta skapar en eventlistener på meny ikonen när användaren klickar på knappen */
+		toggleNav(); /* Kallar togleNav() funktionen */
 	});
 
-	const toggleNav = () => {
-		if (body.classList.contains("nav-active")) body.classList.remove("nav-active");
+	const toggleNav = () => { 
+		/* Denna lägger till, alternativt tar bort klassen "nav-active"  från bodyn 
+			vilket tillåter mig att huvudsakligen använda css för navigationen*/
+		if (body.classList.contains("nav-active")) body.classList.remove("nav-active"); /* Lägger till klassen om klassen inte redan finns, annars tar den bort den */
 		else body.classList.add("nav-active");
 	};
 };
 
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener("DOMContentLoaded", init); /* Detta kallar funktionen init() när sidan laddas */
